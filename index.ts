f3966f7dc3cbeb999ca22bf789e92a91dd8aedf7
Bryan <Brayanson33@gmail.com>
2026-01-03 13:53:50 -0500

    Modificacion del puerto

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 const server = Bun.serve({
-    hostname: "0.0.0.0",
-    port: Number(process.env.PORT) || 3000,
-    fetch(req) {
+    port: process.env.PORT ?? 3000,
+    async fetch(req) {
         return new Response("API de Bun esta funcionando correctamente");
     },
 });
 
-console.log(`Servidor corriendo en puerto ${server.port}`);
+console.log(`Servidor corriendo en ${server.url}:${server.port}`);
